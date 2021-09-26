e3387686828e7003af27f078f66cd0a0946b11df
Denis Venher <vendenven@gmail.com>
2021-09-26 21:48:04 +0300

    add mob markup

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mob-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
